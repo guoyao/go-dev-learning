c8eda9db6911ed6a28f06d9411dc195375944a79
guoyao <guoyao.dev@gmail.com>
2013-07-02 18:33:48 +0800

    add upload func

--- go-web-learning/src/main.go
+++ go-web-learning/src/main.go
@@ -17,6 +17,7 @@ import (
 	"crypto/md5"
 	"io"
 	"strconv"
+	"os"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -57,9 +58,40 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// 处理/upload 逻辑
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./uploads/" + handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", sayhelloName)       //设置访问的路由
 	http.HandleFunc("/login", login)         //设置访问的路由
+	http.HandleFunc("/upload", upload)
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
