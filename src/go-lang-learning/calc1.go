047b87c0143703c2241c3bf71f60af301ff4c446
wuguoyao <wuguoyao@baidu.com>
2015-10-25 22:40:50 +0800

    update

--- src/go-lang-learning/calc1.go
+++ src/go-lang-learning/calc1.go
@@ -1,14 +1,39 @@
 package main
 
 import "fmt"
-import "time"
+import _ "time"
+import "reflect"
+
+type Bird struct {
+	Name           string
+	LifeExpectance int
+}
+
+func (b *Bird) Fly() {
+	fmt.Println("I am flying...")
+}
 
 func main() {
-	t1 := time.Now()
-	count := 0
-	for i := 0; i < 9000000000; i++ {
-		count += i
+	/*
+		t1 := time.Now()
+		count := 0
+		for i := 0; i < 9000000000; i++ {
+			count += i
+		}
+		t2 := time.Now()
+		fmt.Printf("cost:%d, count:%d\n", t2.Sub(t1), count)
+	*/
+
+	// arr := []int{1, 2, 3}
+
+	sparrow := &Bird{"Sparrow", 3}
+	s := reflect.ValueOf(sparrow).Elem()
+	typeOfT := s.Type()
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		fmt.Printf("%d: %s %s = %v\n", i, typeOfT.Field(i).Name, f.Type(), f.Interface())
 	}
-	t2 := time.Now()
-	fmt.Printf("cost:%d, count:%d\n", t2.Sub(t1), count)
+
+	var nu int = 10
+	fmt.Println(string(nu))
 }
