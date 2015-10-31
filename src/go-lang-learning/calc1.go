dc8590449c9290f7c5cd1b8164a05b08f42d60ca
guoyao <guoyao.dev@gmail.com>
2015-10-31 11:20:49 +0800

    update

--- src/go-lang-learning/calc1.go
+++ src/go-lang-learning/calc1.go
@@ -1,39 +1,94 @@
 package main
 
 import "fmt"
-import _ "time"
-import "reflect"
+import "os"
+import "io"
+import "bufio"
 
-type Bird struct {
-	Name           string
-	LifeExpectance int
+func readFile(path string) (result string, err error) {
+	result = ""
+	file, err := os.Open(path)
+
+	defer file.Close()
+
+	if err != nil {
+		fmt.Println("Failed to open file ", path)
+		return
+	}
+
+	br := bufio.NewReader(file)
+
+	for {
+		line, isPrefix, err1 := br.ReadLine()
+
+		if err1 != nil {
+			if err1 != io.EOF {
+				err = err1
+			}
+			break
+		}
+
+		if isPrefix {
+			fmt.Println("A too long line, seems unexpected.")
+			return
+		}
+
+		result += string(line) + "\n"
+	}
+
+	return
 }
 
-func (b *Bird) Fly() {
-	fmt.Println("I am flying...")
+type Slice []int
+
+func (s *Slice) remove(index int) *int {
+	if index < 0 || index >= len(*s) {
+		return nil
+	}
+	(*s)[3] = 0
+	removed := (*s)[index]
+
+	if index > 0 && index < len(*s) {
+		s1 := append((*s)[:index-1], (*s)[index+1:]...)
+		s = &s1
+	} else if index == 0 {
+		s1 := make(Slice, 0)
+		s = &s1
+	} else {
+		s1 := (*s)[:index-1]
+		s = &s1
+	}
+
+	return &removed
 }
 
 func main() {
-	/*
-		t1 := time.Now()
-		count := 0
-		for i := 0; i < 9000000000; i++ {
-			count += i
-		}
-		t2 := time.Now()
-		fmt.Printf("cost:%d, count:%d\n", t2.Sub(t1), count)
-	*/
-
-	// arr := []int{1, 2, 3}
-
-	sparrow := &Bird{"Sparrow", 3}
-	s := reflect.ValueOf(sparrow).Elem()
-	typeOfT := s.Type()
-	for i := 0; i < s.NumField(); i++ {
-		f := s.Field(i)
-		fmt.Printf("%d: %s %s = %v\n", i, typeOfT.Field(i).Name, f.Type(), f.Interface())
+	content, err := readFile("/root/.vimrc")
+	if err == nil {
+		fmt.Println(content)
 	}
 
-	var nu int = 10
-	fmt.Println(string(nu))
+	type Animal struct {
+		Name string
+	}
+
+	dog := new(Animal)
+	dog.Name = "dog"
+	dog2 := dog
+	fmt.Println(dog2 == dog)
+	dog2.Name = "dog2"
+	fmt.Println(dog.Name)
+	fmt.Println(dog2.Name)
+	fmt.Println(dog2 == dog)
+
+	fmt.Println("------------------")
+	slice := &Slice{1, 2, 3, 4, 5}
+	fmt.Println(*slice)
+	slice2 := slice
+	removed := slice2.remove(0)
+	if removed != nil {
+		fmt.Println(*removed)
+	}
+	fmt.Println(*slice)
+	fmt.Println(slice == slice2)
 }
